6a2612fe5553f41f0492e6cef2c72152695494fc
Mitchell Tracy <mittracy7@gmail.com>
2016-04-16 14:41:40 -0400

    started a simple library in parse.py that interfaces with the database

--- src/parse.py
+++ src/parse.py
@@ -1,5 +1,39 @@
+import psycopg2
 
+class Edge:
+	def __init__(self, src, dest, time):
+		self.src = src
+		self.dest = dest
+		self.time = time
+		    
+	def __str__(self):
+		return "Source: " + self.src +", Dest: " + self.dest + ", Time: " + str(self.time)
 
-with open('../Data/measurements', 'r') as f:
-    first_line = f.readline()
-    print(first_line.split( )[0])
+def establish_db_connection():
+	try:
+		conn=psycopg2.connect("dbname='king' user='cmsc711' host='localhost' password='cmsc711'") 
+		return conn
+	except Exception as e:
+		print("I am unable to connect to the database.")
+		print(e)
+
+def match_on_src(src, cur):
+	cur.execute("SELECT * FROM Measurements WHERE src = %s", (src,))
+	results = []
+	for record in cur:
+		results.append(Edge(record[0], record[1], int(record[2])))
+	return results
+	
+def match_on_dest(dest, cur):
+	cur.execute("SELECT * FROM Measurements WHERE dest = %s", (dest,))
+	results = []
+	for record in cur:
+		results.append(Edge(record[0], record[1], int(record[2])))
+	return results
+	
+def get_edges(x, y, cur):
+	cur.execute("SELECT * FROM Measurements WHERE (src = %s AND dest = %s) OR (src = %s AND dest = %s)", (x, y, y, x, ))
+	results = []
+	for record in cur:
+		results.append(Edge(record[0], record[1], int(record[2])))
+	return results
---
+++ src/triangulate.py
@@ -0,0 +1,18 @@
+import parse
+
+conn = parse.establish_db_connection()
+cur = conn.cursor()
+testsrc = "12.10.83.40"
+dests = parse.match_on_src(testsrc, cur);
+#for d in dests:
+#	print(d)
+
+testdest = "12.10.83.40"
+srcs = parse.match_on_dest(testdest, cur);
+#for d in srcs:
+#	print(d)
+
+testother = "12.104.199.65"
+edges = parse.get_edges(testsrc, testdest, cur)
+for d in edges:
+	print(d)
