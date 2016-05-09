7f6242f473e4de25afea37dc5811efc3603de382
Nikofinas <nikofinas@gmail.com>
2016-05-09 00:56:41 -0400

    Update evaluation code.
    Signed-off-by: Nikofinas <nikofinas@gmail.com>

--- test/evaluate.py
+++ test/evaluate.py
@@ -11,74 +11,76 @@ import matplotlib.pyplot as plt
 def evaluate_triangulate():
 	accuracy = []
 	error = []
-	with open('config.json') as config_file:    
+	with open('test_tri_config.json') as config_file:
 		configs = json.load(config_file)
-	t = tri.TriangulationNet()
-	clients = configs['clients']
-	servers = configs['servers']
-	t.bootstrap(servers[0], servers[1], servers[2])
-	total_num = len(servers)
-	i = 3
-	numbers = []
-	for c in clients:
-		t.add_client(c)
-	a, e = t.calculate_accuracy()
-	accuracy.append(a)
-	error.append(e)
-	numbers.append(i)
-	
-	servers = servers[3:len(servers)]
-	
-	for s in servers:
-		t.add_server(s)
-		a, e = t.calculate_accuracy()
-		accuracy.append(a)
-		error.append(e)
-		i = i+1
-		numbers.append(i)
-	
-	f1 = plt.figure(1)
-	p1 = plt.plot(numbers, accuracy)
-	plt.setp(p1, linewidth=5, color="g", linestyle='--')
-	p2 = plt.plot(numbers, error)
-	plt.setp(p2, linewidth=5, color="r", linestyle='--')
-	plt.title("Accuracy During Server Addition")
-	plt.xlabel("Number of i3 Servers")
-	plt.ylabel("Accuracy (Green) / Relative Error (Red)")
-	plt.axis([3, total_num, 0, 1])
-	#print(accuracy)
-	#print(error)
-	"""
-	accuracy = []
-	numbers = []
-	error = []
 
-	servers = [bootstraps[0], bootstraps[1], bootstraps[2]] + servers
-	
-	toremove = servers[:len(servers) - 3]
-	
-	for s in toremove:
-		i=i-1
-		numbers.append(i)
-		t.remove_server(s)
+	cfg = 1
+	for config in configs["tests"]:
+		t = tri.TriangulationNet()
+		clients = config['clients']
+		servers = config['servers']
+		t.bootstrap(servers[0], servers[1], servers[2])
+		total_num = len(servers)
+		i = 3
+		numbers = []
+		for c in clients:
+			t.add_client(c)
 		a, e = t.calculate_accuracy()
 		accuracy.append(a)
 		error.append(e)
-	
-	f2 = plt.figure(2)
-	line = plt.plot(numbers, accuracy)
-	plt.setp(line, linewidth=5, color="g", linestyle='--')
-	line = plt.plot(numbers, error)
-	plt.setp(line, linewidth=5, color="r", linestyle='--')
-	plt.title("Accuracy During Server Removal")
-	plt.xlabel("Number of i3 Servers")
-	plt.ylabel("Accuracy (Green) / Relative Error (Red)")
-	"""
-	plt.show()
-	
-	
-	
-	
+		numbers.append(i)
+
+		servers = servers[3:len(servers)]
+
+		for s in servers:
+			t.add_server(s)
+			a, e = t.calculate_accuracy()
+			accuracy.append(a)
+			error.append(e)
+			i = i+1
+			numbers.append(i)
+
+		f1 = plt.figure(cfg)
+		p1 = plt.plot(numbers, accuracy)
+		plt.setp(p1, linewidth=5, color="g", linestyle='--')
+		p2 = plt.plot(numbers, error)
+		plt.setp(p2, linewidth=5, color="r", linestyle='--')
+		plt.title("Accuracy During Server Addition" + str(cfg))
+		plt.xlabel("Number of i3 Servers")
+		plt.ylabel("Accuracy (Green) / Relative Error (Red)")
+		plt.axis([3, total_num, 0, 1])
+		plt.show()
+
+		cfg = cfg + 1
+
+		"""
+		accuracy = []
+		numbers = []
+		error = []
+
+		servers = [bootstraps[0], bootstraps[1], bootstraps[2]] + servers
+
+		toremove = servers[:len(servers) - 3]
+
+		for s in toremove:
+			i=i-1
+			numbers.append(i)
+			t.remove_server(s)
+			a, e = t.calculate_accuracy()
+			accuracy.append(a)
+			error.append(e)
+
+		f2 = plt.figure(2)
+		line = plt.plot(numbers, accuracy)
+		plt.setp(line, linewidth=5, color="g", linestyle='--')
+		line = plt.plot(numbers, error)
+		plt.setp(line, linewidth=5, color="r", linestyle='--')
+		plt.title("Accuracy During Server Removal")
+		plt.xlabel("Number of i3 Servers")
+		plt.ylabel("Accuracy (Green) / Relative Error (Red)")
+		plt.show()
+		"""
+
 
 
 evaluate_triangulate()
--- test/evaluate_location_based.py
+++ test/evaluate_location_based.py
@@ -9,53 +9,30 @@ import matplotlib.pyplot as plt
 def evaluate_location():
 	accuracy = []
 	error = []
-	with open('config.json') as config_file:    
+	with open('test_loc_config.json') as config_file:
 		configs = json.load(config_file)
-	
-	clients = configs['clients']
-	all_servers = configs['servers']
-	total_num = len(all_servers)
-	
+
 	conn = parse.establish_db_connection()
 	cur = conn.cursor()
-	
-	servers = all_servers[0:3]
-	
-	all_servers = all_servers[3:len(all_servers)]
-	i = 3
-	numbers = []
-	accuracy = []
-	error = []
-	
-	correct = 0.0
-	e = 0.0
-	total = 0.0
-		
-	for client in clients:
-		dist_min = parse.get_closest_geo_from_subset(client, servers, cur)
-		actual_min = parse.get_actual_closest_from_subset(client, servers, cur)
-		if dist_min != None and actual_min != None:
-			total = total + 1
-		else:
-			continue
-			
-		if dist_min[1] == actual_min[1]:
-			correct = correct + 1
-		else:
-			e = e + (float(dist_min[3]) - float(actual_min[2])) / float(dist_min[3])
-	accuracy.append(correct / total)
-	error.append(e / total)
-	numbers.append(i)
-	i= i+1
-	
-	
-	for server in all_servers:
-		
-		servers.append(server)
+
+	cfg = 1
+	for config in configs["tests"]:
+		clients = config['clients']
+		all_servers = config['servers']
+		total_num = len(all_servers)
+
+		servers = all_servers[0:3]
+
+		all_servers = all_servers[3:len(all_servers)]
+		i = 3
+		numbers = []
+		accuracy = []
+		error = []
+
 		correct = 0.0
 		e = 0.0
 		total = 0.0
-		
+
 		for client in clients:
 			dist_min = parse.get_closest_geo_from_subset(client, servers, cur)
 			actual_min = parse.get_actual_closest_from_subset(client, servers, cur)
@@ -63,7 +40,7 @@ def evaluate_location():
 				total = total + 1
 			else:
 				continue
-				
+
 			if dist_min[1] == actual_min[1]:
 				correct = correct + 1
 			else:
@@ -71,25 +48,43 @@ def evaluate_location():
 		accuracy.append(correct / total)
 		error.append(e / total)
 		numbers.append(i)
-		i= i+1
+		i = i + 1
 
 
-	p1 = plt.plot(numbers, accuracy)
-	plt.setp(p1, linewidth=5, color="g", linestyle='--')
-	p2 = plt.plot(numbers, error)
-	plt.setp(p2, linewidth=5, color="r", linestyle='--')
-	plt.title("Accuracy During Server Addition")
-	plt.xlabel("Number of i3 Servers")
-	plt.ylabel("Accuracy (Green) / Relative Error (Red)")
-	plt.axis([3, total_num, 0, 1])
-	plt.show()
-	"""
-			print ("Client Ip = " + client + "\nClosest Distance\nIp = " + dist_min[1] +
-				"\tLatency = " + str(dist_min[3]) + "\tdist = " + str(dist_min[2]) + 
-				"\nActual Closest\nIp = " + actual_min[1] + "\tLatency = " +
-				str(actual_min[2]) + "\n")
-			print "--------------------"
-	"""
+		for server in all_servers:
+			servers.append(server)
+			correct = 0.0
+			e = 0.0
+			total = 0.0
+
+			for client in clients:
+				dist_min = parse.get_closest_geo_from_subset(client, servers, cur)
+				actual_min = parse.get_actual_closest_from_subset(client, servers, cur)
+				if dist_min != None and actual_min != None:
+					total = total + 1
+				else:
+					continue
+
+				if dist_min[1] == actual_min[1]:
+					correct = correct + 1
+				else:
+					e = e + (float(dist_min[3]) - float(actual_min[2])) / float(dist_min[3])
+			accuracy.append(correct / total)
+			error.append(e / total)
+			numbers.append(i)
+			i = i+1
+
+		f1 = plt.figure(cfg)
+		p1 = plt.plot(numbers, accuracy)
+		plt.setp(p1, linewidth=5, color="g", linestyle='--')
+		p2 = plt.plot(numbers, error)
+		plt.setp(p2, linewidth=5, color="r", linestyle='--')
+		plt.title("Accuracy During Server Addition config: " + str(cfg))
+		plt.xlabel("Number of i3 Servers")
+		plt.ylabel("Accuracy (Green) / Relative Error (Red)")
+		plt.axis([3, total_num, 0, 1])
+		plt.show()
+		cfg = cfg + 1
 
 evaluate_location()
 
