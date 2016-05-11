0b33897d55751f08ff138b13f42a133f0595b7a1
Mitchell Tracy <mittracy7@gmail.com>
2016-05-10 22:27:52 -0400

    modified evaluate to take the average of the tests

--- test/evaluate.py
+++ test/evaluate.py
@@ -8,32 +8,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
+def get_avg(elt, num):
+	return elt / num
+
 def evaluate_triangulate():
 
 	pp = PdfPages('accuracy_tri.pdf')
 
-	with open('test_tri_config.json') as config_file:
+	with open('test_loc_config.json') as config_file:
 		configs = json.load(config_file)
 
 	cfg = 1
+	accuracy = [0]*(len(configs["tests"][0]["servers"])-2)
+	error = [0]*(len(configs["tests"][0]["servers"])-2)
 	for config in configs["tests"]:
-		accuracy = []
-		error = []
-
+		print(len(accuracy))
+		print(cfg)
 		t = tri.TriangulationNet()
 		clients = config['clients']
 		servers = config['servers']
 		t.bootstrap(servers[0], servers[1], servers[2])
 		total_num = len(servers)
-		i = 3
+		i = 0
 		numbers = []
 		for c in clients:
 			print "Client: " + c
 			t.add_client(c)
 		a, e = t.calculate_accuracy()
-		accuracy.append(a)
-		error.append(e)
-		numbers.append(i)
+		
+		
+		accuracy[i] = accuracy[i] + a
+		error[i] = error[i] + e
 
 		servers = servers[3:len(servers)]
 
@@ -41,25 +46,36 @@ def evaluate_triangulate():
 			print "Server: " + s
 			t.add_server(s)
 			a, e = t.calculate_accuracy()
-			accuracy.append(a)
-			error.append(e)
 			i = i+1
-			numbers.append(i)
-
-		f1 = plt.figure(cfg)
-		p1 = plt.plot(numbers, accuracy)
-		plt.setp(p1, linewidth=5, color="g", linestyle='--')
-		p2 = plt.plot(numbers, error)
-		plt.setp(p2, linewidth=5, color="r", linestyle='--')
-		plt.title("Accuracy During Server Addition Config: " + str(cfg))
-		plt.xlabel("Number of i3 Servers")
-		plt.ylabel("Accuracy (Green) / Relative Error (Red)")
-		plt.axis([3, total_num, 0, 1])
-		# plt.show()
-		plt.savefig(pp, format='pdf')
-
+			accuracy[i] = accuracy[i] + a
+			error[i] = error[i] + e
+		
 		cfg = cfg + 1
+		
+		
 
+		
+	
+	
+	numbers = range(3, len(accuracy)+3)
+	accuracy = [a / len(configs["tests"]) for a in accuracy]
+	error = [e / len(configs["tests"]) for e in error]
+	
+	print(len(numbers))
+	print(len(accuracy))
+	print(len(error))
+	
+	f1 = plt.figure(cfg)
+	p1 = plt.plot(numbers, accuracy)
+	plt.setp(p1, linewidth=5, color="g", linestyle='--')
+	p2 = plt.plot(numbers, error)
+	plt.setp(p2, linewidth=5, color="r", linestyle='--')
+	plt.title("Average Accuracy During Server Addition")
+	plt.xlabel("Number of i3 Servers")
+	plt.ylabel("Accuracy (Green) / Relative Error (Red)")
+	plt.axis([3, total_num, 0, 1])
+	# plt.show()
+	plt.savefig(pp, format='pdf')
 	pp.close()
 
 
